d74675308c330eeb8d6faea3273ca4aab68d2b32
Imran Ahmed <jappleleaf@gmail.com>
2018-05-18 11:19:08 -0400

    initial setup

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const port = process.env.PORT || 5000;
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/auth-users', {
   useMongoClient: true
-});
+})
+.then(resp => console.log('connected to mongodb'))
+.catch(err => console.log(err));
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
